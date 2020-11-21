c9f466e917f31b6bedbe734f3ca84ff8eb5d9a2a
ShraddhaB <sbhingarde2017@gmail.com>
2020-11-21 11:16:37 +0530

    gulp dev dependencies removed

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require("gulp");
+const gap = require("gulp-append-prepend");
+
+//compile scss into css
+function style() {
+  //1. where is my sass file going to be
+  return gulp.src('./scss/**/*.sass')
+  //2. pass that file through sass compiler
+  .pipe(sass())
+  //3.Where do I save the compiled css
+  .pipe(gulp.dest('./css'))
+}
+exports.default = style
